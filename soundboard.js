ead6276bb7865acce91b1bfd41349cd9e2810254
Keeton Lanzinger <lanzing1@msu.edu>
2022-11-17 15:15:53 -0500

    first commit

---
+++ soundboard.js
@@ -0,0 +1,12 @@
+function play1(){
+    var boing = new Audio('boing.wav');
+    boing.play();
+}
+function play2(){
+    var bubbles = new Audio('bubbles.wav');
+    bubbles.play();
+}
+function play3(){
+    var pins = new Audio('pins.wav');
+    pins.play();
+}
